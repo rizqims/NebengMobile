8ed5824c2e1a45f810bfb59dc6aad38493621e95
rizqims <rizqims100@gmail.com>
2021-12-20 11:16:28 +0700

    tambah function untuk Nawar Nebeng

--- app/src/main/java/com/example/nebeng/Beranda.kt
+++ app/src/main/java/com/example/nebeng/Beranda.kt
@@ -1,8 +1,11 @@
 package com.example.nebeng
 
+//import com.example.nebeng.ui.notifications.NotificationsFragment
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class Beranda : AppCompatActivity() {
 
     private lateinit var binding: ActivityBerandaBinding
-    private lateinit var sharedpref : PreferencesHelper
+    private lateinit var sharedpref: PreferencesHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,29 +41,44 @@ class Beranda : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+//        val akunFrag : View = layoutInflater.inflate(R.layout.fragment_notifications, null)
+//        val homeFrag : View = layoutInflater.inflate(R.layout.fragment_home, null)
+
+
         //beranda
-        val textNama : TextView = findViewById(R.id.namaAkun)
-        val textNoTelp : TextView = findViewById(R.id.noTelp)
+        val textNama: TextView = findViewById(R.id.namaAkun)
+        val textNoTelp: TextView = findViewById(R.id.noTelp)
         textNama.setText(sharedpref.getString(Constant.NAMA_LENGKAP))
         textNoTelp.setText(sharedpref.getString(Constant.NO_TELP))
 
-        //nah ini saya mau nampilin data dihalaman profile cuma error katanya kan beda fragment
-        //sedangkan yg ini fragment beranda / fragment home
-        //profile
+//        Masih belum work, jangan disentuh
 //        val textNamaProfile : TextView = findViewById(R.id.namaProfile)
-//        val textEmailProfile : TextView = findViewById(R.id.emailAkun)
-//        val textNoTelpProfile : TextView = findViewById(R.id.noAkun)
+//        val textEmailProfile : TextView = akunFrag.findViewById(R.id.emailAkun)
+//        val textNoTelpProfile : TextView = akunFrag.findViewById(R.id.noAkun)
 //        textNamaProfile.setText(Constant.NAMA_LENGKAP)
 //        textEmailProfile.setText(Constant.EMAIL)
 //        textNoTelpProfile.setText(Constant.NO_TELP)
 
         //nah ini buat tombol logout cuma ini kan tombol dari beranda bukan yang tombol keluar dari halaman profile
-        val logoutBtn : Button = findViewById(R.id.Logoutbtn)
-        logoutBtn.setOnClickListener{
+//        val logoutBtn: Button = findViewById(R.id.keluarbtn)
+        val logoutBtn: Button = findViewById(R.id.Logoutbtn)
+        logoutBtn.setOnClickListener {
             sharedpref.logout()
-            Toast.makeText(this,"Logout Berhasil",Toast.LENGTH_SHORT)
-            startActivity(Intent(this,HomeActivity::class.java))
+            Toast.makeText(this, "Logout Berhasil", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        val i: ImageButton = findViewById(R.id.Nebengbtn)
+        i.setOnClickListener {
+            val Intent = Intent(this, NebengM::class.java)
+            startActivity(Intent)
+        }
+
+        val o: ImageButton = findViewById(R.id.Nawarbtn)
+        o.setOnClickListener {
+            val Intent = Intent(this, Nawar_M::class.java)
+            startActivity(Intent)
         }
     }
-    //baru sampe session selesai,logout selesai tinggal cari tau ambil textview dari fragment
 }
